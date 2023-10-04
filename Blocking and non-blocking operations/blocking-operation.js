c9c67cac60ed60cd4edd22c538af36b6d91e09fc
anuitexDev <dev@anuitex.net>
2023-10-04 13:08:20 +0300

    added examples

---
+++ Blocking and non-blocking operations/blocking-operation.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("./text.txt", "utf-8");
+
+console.log("File reading finished");
+console.log(data);
+
+console.log("Continue...");
---
+++ Blocking and non-blocking operations/non-blocking-operation.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+fs.readFile("./text.txt", "utf-8", (err, data) => {
+  if(!err) {
+    console.log("File reading finished");
+    console.log(data);
+  }
+});
+
+console.log("Continue...");
